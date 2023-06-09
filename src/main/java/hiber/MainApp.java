3abac7a55c7cf8af1f8b4fdf846ad7067f9db860
Vadim <kosmonaft2007@ya.ru>
2023-06-09 11:39:26 +0300

    first commit5

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -19,18 +19,17 @@ public class MainApp {
         UserService userService = context.getBean(UserService.class);
 
         userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Mers", 350)));
-        userService.add( new User("User2", "Lastname2", "user2@mail.ru", new Car("Bexa", 725)));
-        userService.add( new User("User3", "Lastname3", "user3@mail.ru", new Car("Tazic", 2112)));
-        userService.add( new User("User4", "Lastname4", "user4@mail.ru", new Car("Cruzak", 200)));
-
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Bexa", 725)));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Tazic", 2112)));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Cruzak", 200)));
 
 
         List<User> users = userService.listUsers();
         for (User user : users) {
-            System.out.println("Id = "+user.getId());
-            System.out.println("First Name = "+user.getFirstName());
-            System.out.println("Last Name = "+user.getLastName());
-            System.out.println("Email = "+user.getEmail());
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
             System.out.println();
         }
         System.out.println("The car belongs to the user with the data: " + userService.getUserByCar("Mers", 350));
